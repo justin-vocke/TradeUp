90aec7f179184d9c01da5b19150b837e0f8acbe5
Justin Vocke <therealmathhelper@gmail.com>
2024-11-24 11:24:10 -0600

    Add back controllers folder and basic notification and user model.

---
+++ TradeUp.Api/Controllers/StocksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TradeUp.Api.Controllers
+{
+    [Route("api/stocks")]
+    [ApiController]
+    public class StocksController : ControllerBase
+    {
+    }
+}
--- TradeUp.Domain.Core/Entities/User.cs
+++ TradeUp.Domain.Core/Entities/User.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace TradeUp.Domain.Core.Entities
 {
-    internal class Notification
+    internal class User
     {
-        public DateTime CreatedAt { get; set; }
     }
 }
