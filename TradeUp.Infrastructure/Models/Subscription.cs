a03eb02ee4af5431818c815fae6a1d9e0096b645
Justin Vocke <therealmathhelper@gmail.com>
2024-12-13 15:24:06 -0600

    add db context and User/Subscription models.

---
+++ TradeUp.Infrastructure/Models/Subscription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeUp.Infrastructure.Models
+{
+    public class Subscription
+    {
+        [Key]
+        public int SubscriptionId { get; set; }
+        public decimal Threshold { get; set; }
+        public string Ticker { get; set; }
+
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ TradeUp.Infrastructure/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeUp.Infrastructure.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; }
+
+        public IEnumerable<Subscription> Subscriptions { get; set; }
+    }
+}
