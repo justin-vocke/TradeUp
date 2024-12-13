24cfba647db63d63ea087bf81cb73a61815f1c55
Justin Vocke <therealmathhelper@gmail.com>
2024-12-13 11:57:33 -0600

    Inject price threshold event and event handler with DI.

---
+++ TradeUp.Application/EventHandlers/PriceThresholdReachedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tradeup.Domain.Core.Bus;
+using TradeUp.Domain.Core.Events;
+
+namespace TradeUp.Application.EventHandlers
+{
+    public class PriceThresholdReachedEventHandler : IEventHandler<PriceThresholdReachedEvent>
+    {
+        //want to send email to user
+        public Task Handle(PriceThresholdReachedEvent @event)
+        {
+            Console.WriteLine("Email sent");
+            var test = "stop here";
+            return Task.CompletedTask;
+        }
+    }
+}
