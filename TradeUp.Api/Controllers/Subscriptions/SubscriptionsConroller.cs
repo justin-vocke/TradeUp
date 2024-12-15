ab33d30c4201b07f90cbc5508af0af1314d420e8
Justin Vocke <therealmathhelper@gmail.com>
2024-12-15 11:44:31 -0600

    Add controller and command/command handler basic logic.

---
+++ TradeUp.Api/Controllers/Subscriptions/SubscriptionsConroller.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TradeUp.Application.Commands.Subscriptions;
+
+namespace TradeUp.Api.Controllers.Subscriptions
+{
+    [Route("api/subscriptions")]
+    [ApiController]
+    public class SubscriptionsConroller : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public SubscriptionsConroller(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSubscription(
+            CreateSubscriptionRequest request,
+            CancellationToken cancellationToken)
+        {
+            var command = new CreateSubscriptionCommand
+            {
+                UserId = request.UserId,
+                Threshold = request.Threshold,
+                Ticker = request.Ticker,
+            };
+
+            var result = await _sender.Send(command, cancellationToken);
+            return Ok();
+        }
+    }
+}
---
+++ TradeUp.Application/Commands/Subscriptions/CreateSubscriptionCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeUp.Domain.Core.Entities;
+
+namespace TradeUp.Application.Commands.Subscriptions
+{
+    public class CreateSubscriptionCommand : Command
+    {
+        public Guid UserId { get; set; }
+        public decimal Threshold { get; set; }
+        public string Ticker { get; set; }
+    }
+}
