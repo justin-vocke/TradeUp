51731fdef0b640ab50ae97ce2341148657f39b91
Justin Vocke <therealmathhelper@gmail.com>
2024-12-15 10:55:10 -0600

    Add skeleton for command, command handler, and user repository

--- TradeUp.Application/Commands/Users/CreateUserCommand.cs
+++ TradeUp.Application/Commands/Users/CreateUserCommand.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TradeUp.Domain.Core.Entities;
 
-namespace TradeUp.Domain.Core.Entities
+namespace TradeUp.Application.Commands.Users
 {
-    public class User
+    public class CreateUserCommand : Command
     {
         public Guid UserId { get; set; }
         public string Email { get; set; }
