bab8e7f69e090fde0983d8c5f54e9ab44690c98e
Justin Vocke <therealmathhelper@gmail.com>
2024-12-16 13:47:05 -0600

    Add result and error type. Add create user command and it's handler.

---
+++ TradeUp.Application/Commands/Users/CreateUserCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeUp.Application.Interfaces;
+using TradeUp.Domain.Core.Entities;
+
+namespace TradeUp.Application.Commands.Users
+{
+    public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand, Guid>
+    {
+        public Task<Result<Guid>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TradeUp.Domain.Core/Entities/Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeUp.Domain.Core.Entities
+{
+    public record Error(string Code, string Name)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+
+        public static readonly Error NullValue = new("Error.NullValue", "Null value was provided");
+    }
+}
